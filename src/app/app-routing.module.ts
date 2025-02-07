7460d42742095d43fb869cb469af274b4b935e34
StevenGerman <germansteven2024@gmail.com>
2025-02-07 15:55:54 -0300

    rutaeditar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { EditorialListarComponent } from './components/pages/editoriales/editorial-listar/editorial-listar.component';
 import { EditorialAgregarComponent } from './components/pages/editoriales/editorial-agregar/editorial-agregar.component';
+import { EditorialEditarComponent } from './components/pages/editoriales/editorial-editar/editorial-editar.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'editorial-listar', component: EditorialListarComponent},
   {path: 'editorial-agregar', component: EditorialAgregarComponent},
+  {path: 'editorial-editar:id', component: EditorialEditarComponent},
 ];
 
 @NgModule({
