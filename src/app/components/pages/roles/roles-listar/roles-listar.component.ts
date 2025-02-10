fe13efe4d16ddc7ebe0783f4eb5ef33e6a54e364
StevenGerman <germansteven2024@gmail.com>
2025-02-10 01:34:31 -0300

    a

--- src/app/components/pages/roles/roles-listar/roles-listar.component.ts
+++ src/app/components/pages/roles/roles-listar/roles-listar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Rol } from 'src/app/interfaces/rol.model';
+import { RolService } from 'src/app/services/rol.service';
 
 @Component({
   selector: 'app-roles-listar',
   templateUrl: './roles-listar.component.html',
   styleUrls: ['./roles-listar.component.css']
 })
-export class RolesListarComponent {
+export class RolesListarComponent implements OnInit {
+
+
+  listadoRoles:Rol[]= []
+
+  constructor(
+
+    private servicioRoles:RolService,
+  ){
+    this.cargarRoles();
+  }
+
+  ngOnInit(){}
+
+  cargarRoles(){
+    this.servicioRoles.obtenerRoles().subscribe((respuesta)=>{
+      console.log(respuesta);
+      this.listadoRoles = respuesta;
+    })
+  }
+
+  eliminarRol(idRol:any){
+    
+  }
 
 }
