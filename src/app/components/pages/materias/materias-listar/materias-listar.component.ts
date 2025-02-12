91cde7683aeb04b364f092cf63ffab52e9cc1d5d
StevenGerman <germansteven2024@gmail.com>
2025-02-11 23:19:53 -0300

    elimimamat

--- src/app/components/pages/materias/materias-listar/materias-listar.component.ts
+++ src/app/components/pages/materias/materias-listar/materias-listar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Materia } from 'src/app/interfaces/materia.model';
 import { MateriaService } from 'src/app/services/materia.service';
 
@@ -12,6 +13,7 @@ export class MateriasListarComponent implements OnInit {
 
   listadoMaterias:Materia[]=[];
   constructor(
+    private ruteador: Router,
     private servicioMaterias:MateriaService,
   ){}
 
@@ -30,7 +32,11 @@ export class MateriasListarComponent implements OnInit {
   eliminarMateria(idMateria:any){
     this.servicioMaterias.eliminarEditorial(idMateria).subscribe((respuesta)=>{
       console.log(respuesta);
+      
+      this.cargarMaterias();
+      
     })
+   
   }
 
 
