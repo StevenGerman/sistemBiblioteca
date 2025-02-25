83b3221122a35b9eaf561ce150dd233fbde6b017
StevenGerman <germansteven2024@gmail.com>
2025-02-25 18:21:32 -0300

    get-headrs-materias

--- src/app/services/materia.service.ts
+++ src/app/services/materia.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Materia } from '../interfaces/materia.model';
@@ -13,7 +13,8 @@ export class MateriaService {
   constructor(private clienteHttp:HttpClient) { }
 
   obtenerMaterias():Observable<Materia[]>{
-    return this.clienteHttp.get<Materia[]>(this.apiUrl);
+    const headers = new HttpHeaders().set("Authorization", `Bearer ${localStorage.getItem('token')}`);
+    return this.clienteHttp.get<Materia[]>(this.apiUrl,{ headers });
   }
 
   obtenerMateriaID():Observable<Materia>{
